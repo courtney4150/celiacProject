249ec49e3c7d29c38e2131b519beb5aaed27342a
Courtney Sheridan <courtneyrsheridan@gmail.com>
2022-07-01 14:55:58 -0400

    json data file

---
+++ src/app/food-item/food-item.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AppComponent } from '../app.component';
+
+import foodsJson from 'src/app/food-list.json';
+
+interface Food {
+  id: number;
+  name: string;
+  manufacturer: string;
+  description: string;
+}
+
+@Component({
+  selector: 'app-food-item',
+  templateUrl: './food-item.component.html',
+  styleUrls: ['./food-item.component.scss']
+})
+export class FoodItemComponent implements OnInit {
+
+  foods: Food[] = foodsJson;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getFood();
+  }
+
+  getFood(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    
+  }
+
+}
