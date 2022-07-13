b5a3df25bd6f889453418bbc16e3e5675bf48694
Courtney Sheridan <courtneyrsheridan@gmail.com>
2022-07-13 16:03:30 -0400

    search works!!!!

--- src/app/components/top-layout/top-layout.component.ts
+++ src/app/components/top-layout/top-layout.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import foodsJson from 'src/app/food-list.json';
+//import foodsJson from 'src/app/food-list.json';
 
 interface Food {
   id: number;
@@ -15,9 +16,26 @@ interface Food {
   styleUrls: ['./top-layout.component.css']
 })
 export class TopLayoutComponent {
-  foods: Food[] = foodsJson;
+  //foods: Food[] = foodsJson;
+  searchText = '';
+  foodsList: Food[] = [];
+  allFoodsList: Food[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Food[]>('/assets/food-list.json')
+      .subscribe((data: Food[]) => {
+        this.foodsList = data;
+        this.allFoodsList = this.foodsList;
+      })
+  }
 
   onSearch(searchText: string): void {
     console.log(searchText);
+    this.foodsList = this.allFoodsList.filter((val) => 
+    val.name.toLowerCase().includes(searchText)
+    );
   }
 }
